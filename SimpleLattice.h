e3c990ec8ebcae6372b6dffa81170f663a1b5d1f
JHildebrand28 <134550316+JHildebrand28@users.noreply.github.com>
2023-06-08 14:15:13 -0400

    Header file for the 8q lattice

---
+++ SimpleLattice.h
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+#include <complex>
+#include <math.h>
+
+class Lattice {
+    int N; 
+    float beta1;
+    float beta2;
+    float u0;
+
+    public:
+        
+
+
+}
